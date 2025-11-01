cf6609f97cc610f4fd4ee67842372431373adbd8
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-11-01 12:51:46 -0400

    waybar gtk fix

--- waybar/swhkd-mode-watch.sh
+++ waybar/swhkd-mode-watch.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
-
 MODE_FILE="$HOME/.config/waybar/swhkd-mode"
-
 [ ! -f "$MODE_FILE" ] && echo -n "normal" > "$MODE_FILE"
+
+# Ignore SIGPIPE - just silently stop writing on broken pipe
+trap '' PIPE
 
 # Initial output
 cat "$MODE_FILE"
-echo ""  # Important: newline for waybar
+echo ""
 
 # Watch and output on changes
 inotifywait -m -e close_write "$MODE_FILE" 2>/dev/null | while read; do
     cat "$MODE_FILE"
-    echo ""  # Important: newline for waybar
+    echo ""
 done
